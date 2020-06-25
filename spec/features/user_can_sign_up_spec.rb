8cbd202a27f099b42d8a74a63ff3a2f1b556d081
Sima <sima.boreiri@gmail.com>
2020-06-25 11:11:24 +0200

    modifies ruby version and adds test to spec file

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,15 +1,18 @@
 require "rails_helper"
 
 feature 'User can sign up' do
-    context 'User needs to sign up to use the app' do
-        before do
-            visit root_path
-            # click_on 'Sign up'
+  context 'User needs to sign up to use the app' do
+    before do
+      visit root_path
+      click_on 'Sign up'
+      fill_in 'Name', with: 'Pontus'
+      fill_in 'Email', with: 'example@example.com'
+      fill_in 'Password', with: 'password123'
+      fill_in 'Password confirmation', with: 'password123'
     end
 
     it 'shows a sign up button' do
-        expect(page).to have_link 'Sign up'
-    end
-    
-end
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end 
+  end
 end
