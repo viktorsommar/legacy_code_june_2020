20d2a62e725aad55c69d2c449985db2560096151
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-25 12:13:09 +0200

    Adding a factory user. All tests are passing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe User, type: model do
+RSpec.describe User, type: :model do
     describe 'DB table' do
         it { is_expected.to have_db_column :name }
         it { is_expected.to have_db_column :email }
