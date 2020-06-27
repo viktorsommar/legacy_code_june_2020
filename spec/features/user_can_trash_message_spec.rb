fb873376db95c92bf431a6300858a670a88412e3
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-27 14:11:45 +0200

    Modifying test to instead show count of messages in trash. All tests are passing.

--- spec/features/user_can_trash_message_spec.rb
+++ spec/features/user_can_trash_message_spec.rb
@@ -1,21 +1,20 @@
 require "rails_helper"
 
 feature "User can trash and untrash messages" do
-    let(:user) { FactoryBot.create(:user) }
-    let(:sender) { FactoryBot.create(:user, name: 'Alex', email: 'sender@mail.com') }
-    let(:receiver) { FactoryBot.create(:user, name: 'Sima', email: 'receiver@mail.com') }
+    context "Sender can send message and receiver receives it" do
+    let(:user1) { create(:user, name: 'Alex', email: 'sender@mail.com') }
+    let(:user2) { create(:user, name: 'Sima', email: 'receiver@mail.com') }
     before do
-        login_as(user, scope: :user)
-        sender.send_message(receiver, 'Writing some text for test', 'Subject' )
-        visit root_path
-        click_on "Inbox"
+        message = user1.send_message(user2, 'Writing some text for test', 'Subject' )
+        login_as(user2, scope: :user)
+        visit conversation_path(message.notification_id)
+        click_on "Move to trash"
+        visit mailbox_trash_path
     end
 
-    context "User can trash message" do
-        
-        it 'should have 1 new message in the inbox' do
-          count = receiver.mailbox.inbox.count
-          expect(count).to eq 1
+        it 'should have 1 message in the trash' do
+            count = user2.mailbox.trash.count
+            expect(count).to eq 1
         end
     end
 end
