4bc08bb376b0e36c691f58f38664cdbf2b0cbd8e
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-27 15:01:39 +0200

    Adding trashing and untrashing in the same test. All tests passing, keeping untrashing test for now.

--- spec/features/user_can_trash_message_spec.rb
+++ spec/features/user_can_trash_message_spec.rb
@@ -5,15 +5,22 @@ feature "User can trash and untrash messages" do
     let(:user1) { create(:user, name: 'Alex', email: 'sender@mail.com') }
     let(:user2) { create(:user, name: 'Sima', email: 'receiver@mail.com') }
     before do
-        message = user1.send_message(user2, 'Writing some text for test', 'Subject' )
+        @message = user1.send_message(user2, 'Writing some text for test', 'Subject' )
         login_as(user2, scope: :user)
-        visit conversation_path(message.notification_id)
+        visit conversation_path(@message.notification_id)
         click_on "Move to trash"
-        visit mailbox_trash_path
     end
 
         it 'should have 1 message in the trash' do
             count = user2.mailbox.trash.count
+            expect(count).to eq 1
+        end
+
+        it "receiver untrashes the message" do
+            visit conversation_path(@message.notification_id)
+            click_on "Untrash"
+            visit mailbox_inbox_path
+            count = user2.mailbox.inbox.count
             expect(count).to eq 1
         end
     end
